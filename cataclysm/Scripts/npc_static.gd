ce2ab373faead63091e00fb64c292ca77e895bbe
David S <dshi2008mayo@gmail.com>
2025-08-01 01:08:27 -0400

    stashed changes

--- cataclysm/Scripts/npc_static.gd
+++ cataclysm/Scripts/npc_static.gd
@@ -6,20 +6,20 @@ extends Area2D
 var player_in_range = false
 
 func _ready():
-    label.visible = false
-    interaction_zone.body_entered.connect(_on_body_entered)
-    interaction_zone.body_exited.connect(_on_body_exited)
+	label.visible = false
+	interaction_zone.body_entered.connect(_on_body_entered)
+	interaction_zone.body_exited.connect(_on_body_exited)
 
 func _on_body_entered(body):
- if body is Character:
-  player_in_range = true
+	if body is Character:
+		player_in_range = true
 
 func _on_body_exited(body):
- if body is Character:
-  player_in_range = false
-  label.visible = false
+	if body is Character:
+		player_in_range = false
+		label.visible = false
 
 func _process(_delta):
- if player_in_range and Input.is_action_just_pressed("interact"):
-  label.visible = true
-  label.text = "Hi there! I'm an NPC."
+	if player_in_range and Input.is_action_just_pressed("interact"):
+		label.visible = true
+		label.text = "Hi there! I'm an NPC."
