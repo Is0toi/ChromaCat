5ae4377559c047768316fd38441d56c66f371414
emilyxfang <emily.x.fang@gmail.com>
2025-08-01 16:07:11 -0400

    scene change
    scene change

---
+++ cataclysm/Scripts/door.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+func _on_body_entered(body):
+	if body.is_in_group("player"):
+		var current_path = get_tree().current_scene.scene_file_path
+		var next_path = current_path.replace(
+			str(_get_level_number(current_path)),
+			str(_get_level_number(current_path) + 1)
+		)
+		
+		if ResourceLoader.exists(next_path):
+			get_tree().change_scene_to_file(next_path)
+
+func _get_level_number(path: String) -> int:
+	var file_name = path.get_file()
+	return file_name.trim_prefix("level").trim_suffix(".tscn").to_int()
